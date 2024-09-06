cb7d8c6ded3446a99933cd34d42ca24a2a8a302d
savyajajimoggala <savyajajimoggala@gmail.com>
2024-09-06 15:42:19 +0530

    ServiceCard and Collection card created

--- src/app/WebApp/page.tsx
+++ src/app/WebApp/page.tsx
@@ -1,31 +1,45 @@
-'use client'
-import Homecard from "../ReusableComponents/HomeCategory/page"
+import ServiceCard from "../reusableComponents/Services/AdditonalFeatures";
+import Homecard from "../reusableComponents/HomeCategory/page";
+import CollectionCard from "../reusableComponents/CollectionCard/DiffCollection";
+import offer1 from "../../../public/img/offer1.png"
+import offer2 from "../../../public/img/offer2.png"
 
-function importAll(r:any) {
+function importAll(r: any) {
     let images = {};
-    r.keys().map((item :any) => {
-         images[item.replace('./', '')] = r(item); 
-        });
+    r.keys().map((item: any) => {
+        images[item.replace('./', '')] = r(item);
+    });
     return images;
-  }
+}
 
 const images = importAll(require.context('../../../public/img', false, /\.(png|jpg|svg)$/));
 
 const myArray = Object.values(images);
-const Ecommerce=()=>{
-    return(
-        <div className="flex justify-center items-center flex-wrap flex-auto	 ">
-            {myArray.map((item,index)=>{
-                if(item?.default.src.includes('cat')) {
-                    return(
-                        <Homecard productCount={'15 products'} imagepath={item} category={'hello'} />
-                    )
-                }
-            })}
 
-           
+const Ecommerce = () => {
+    return (
+        <div className="flex justify-center items-center flex-wrap flex-auto ">
+            <div className="flex justify-center items-center flex-wrap flex-auto">
+                <ServiceCard service="Quality Product" icon={"fa-sharp fa-solid fa-check"} />
+                <ServiceCard service="Free Shipping" icon={"fa fa-shipping-fast text-primary"} />
+                <ServiceCard service="14-Day Return" icon={"fas fa-exchange-alt text-primary"} />
+                <ServiceCard service="24/7 Support" icon={"fa fa-phone-volume text-primary"} />
+            </div>
+            <div className="flex justify-center items-center flex-wrap flex-auto">
+                {myArray.map((item) => {
+                    if (item.default.src.includes('cat')) {
+                        return (
+                            <div>
+                                <Homecard productCount={'15 products'} imagepath={item} category={'hello'} />
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+            <div>
+                <CollectionCard imagepath={offer1} text={"20% OF THE ALL ORDER"} paragraph={"Spring Collection"}></CollectionCard>
+            </div>
         </div>
     )
-
 }
 export default Ecommerce
--- src/app/reusableComponents/SideNav/page.tsx
+++ src/app/reusableComponents/SideNav/page.tsx
@@ -1,9 +1,5 @@
 'use client'
 import React, { useState } from 'react';
-<<<<<<< HEAD
-=======
-
->>>>>>> d9e9dbe8ea37c1f5cbf1ff6bdd74300e3a8e2e68
 const SideNav: React.FC = () => {
     const [isMainCollapsed, setIsMainCollapsed] = useState<boolean>(true);
     const [isSubCollapsed, setIsSubCollapsed] = useState<boolean>(true);
@@ -16,12 +12,10 @@ const SideNav: React.FC = () => {
         setIsSubCollapsed(prevState => !prevState);
     };
 
-<<<<<<< HEAD
     const handleItemClick = (itemName: string) => {
-        console.log(`Clicked on ${itemName}`);
     };
     return (
-        <div>
+        <div className='w-3/12'>
             <a
                 href="#collapse1"
                 onClick={(e) => {
@@ -29,12 +23,12 @@ const SideNav: React.FC = () => {
                     toggleMainCollapse();
                 }}
             >
-                <h1 className="bg-pink-600 border-neutral-200 border-2 w-64 h-12 flex justify-between items-center">
+                <h1 className="bg-red-300 border-neutral-200 border-2 w-full h-16 pt-0 pr-7	 flex justify-around items-center">
                     Category
                     <i className="fa-solid fa-angle-down float-right"></i>
                 </h1>
             </a>
-            <div id="collapse1" className={isMainCollapsed ? 'collapse' : ''}>
+            <div id="collapse1" className={isMainCollapsed ? '' : ''}>
                 <a
                     href="#collapse2"
                     onClick={(e) => {
@@ -42,108 +36,60 @@ const SideNav: React.FC = () => {
                         toggleSubCollapse();
                     }}
                 >
-                    <h2 className='hover:text-pink-600 border-neutral-200 border-2 w-64 h-12 flex justify-between items-center'>
+                    <h2 className='hover:text-red-300 border-neutral-200 border-2 w-full h-10  pt-0 pr-7 flex justify-around items-center'>
                         Dresses
-                    <i className="fa-solid fa-angle-down float-right"></i>
+                    <i className="fa-solid fa-angle-down float-right "></i>
                     </h2>
                 </a>
                     <div  id="collapse2" className={isSubCollapsed ? 'collapse' : ''} >
                     <ul className='list-none p-0 m-0 absolute'>
 
-                        <li className='bg-purple-100 w-64 hover:bg-gray-50' onClick={() => handleItemClick("Men's Dresses")}>
+                        <li className='bg-purple-100  h-10 hover:bg-gray-50 text-base w-full' onClick={() => handleItemClick("Men's Dresses")}>
                             <a href='#'onClick={()=>{
                                 toggleSubCollapse();
-                            }} className='block p-2'>Men's Dresses</a>
+                            }} className='block p-2 ml-12'>Men's Dresses</a>
                         </li>
-                        <li className='bg-purple-100 w-64 hover:bg-gray-50' onClick={() => handleItemClick("Women's Dresses")}>
+                        <li className='bg-purple-100 w-full h-10 hover:bg-gray-50 text-base' onClick={() => handleItemClick("Women's Dresses")}>
                             <a href='#' onClick={()=>{
                                 toggleSubCollapse();
-                            }} className='block p-2'>Women's Dresses</a>
+                            }} className='block p-2 ml-12'>Women's Dresses</a>
                         </li>
-                        <li className='bg-purple-100 w-64 hover:bg-gray-50' onClick={() => handleItemClick("Baby's Dresses")}>
+                        <li className='bg-purple-100 w-full h-10 hover:bg-gray-50 text-base' onClick={() => handleItemClick("Baby's Dresses")}>
                             <a href='#'onClick={()=>{
                                 toggleSubCollapse();
-                            }} className='block p-2'>Baby's Dresses</a>
+                            }} className='block p-2 ml-12'>Baby's Dresses</a>
                         </li>
                     </ul>
                     </div>
-                <ul className="list-none p-0 m-0 ">
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Jeans</a>
+                <ul className="list-none p-0 m-0">
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Jeans</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Swimwear</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Swimwear</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Sleepwear</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Sleepwear</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Sportswear</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Sportswear</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Jumpsuit</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Jumpsuits</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Blazers</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Blazers</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Jeans</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 '>
+                        <a href='#' className='block p-2 ml-12 text-base'>Jeans</a>
                     </li>
-                    <li className='hover:text-pink-600 border-neutral-200 border-2 w-64'>
-                        <a href='#' className='block p-2'>Shoes</a>
+                    <li className='hover:text-red-300 border-neutral-200 border-2 w-full h-10 text-base	'>
+                        <a href='#' className='block p-2 ml-12'>Shoes</a>
                     </li>
-=======
-    return (
-        <div>
-            <h1>
-                <a
-                    href="#collapse1"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        toggleMainCollapse();
-                    }}
-                    className='bg-rose-400'
-                >
-                    Category
-                </a>
-            </h1>
-            <div id="collapse1" className={isMainCollapsed ? 'collapse' : ''}>
-                <h2>
-                    <a 
-                        href="#collapse2"
-                        onClick={(e) => {
-                            e.preventDefault();
-                            toggleSubCollapse();
-                        }} 
-                    >
-                        Dresses
-                    </a>
-                </h2>
-                <div id="collapse2" className= {isSubCollapsed ? 'collapse' : ''} >
-                    <ul className="border-separate">
-                        <li>Men's Dresses</li>
-                        <li>Women's Dresses</li>
-                        <li>Baby's Dresses</li>
-                    </ul>
-                </div>
-                <ul className="">
-                    <li className='border-sky-500 border-2'>Jeans</li>
-                    <li>Swimwear</li>
-                    <li>Sleepwear</li>
-                    <li>Sportswear</li>
-                    <li>Jumpsuit</li>
-                    <li>Blazers</li>
-                    <li>Jackets</li>
-                    <li>Shoes</li>
->>>>>>> d9e9dbe8ea37c1f5cbf1ff6bdd74300e3a8e2e68
                 </ul>
             </div>
         </div>
     );
 };
 
-<<<<<<< HEAD
 export default SideNav;
-=======
-export default SideNav;
->>>>>>> d9e9dbe8ea37c1f5cbf1ff6bdd74300e3a8e2e68
