1b206558889b11b836744fdaca78f8ccf73ad513
savyajajimoggala <savyajajimoggala@gmail.com>
2024-09-09 15:36:24 +0530

    contact page created

--- src/app/reusableComponents/CollectionCard/page.tsx
+++ src/app/reusableComponents/CollectionCard/page.tsx
@@ -1,8 +1,10 @@
 //@ts-ignore
 import Image from "next/image";
 import offer1 from "../../../../public/img/offer1.png"
+import MailContent from "../CardComponents/MailCard";
 const CollectionCard = ({ imagepath, text, paragraph }) => {
     return (
+        <div>
         <div className=" bg-slate-100 border-2ws w-6/12  ">
             <div className="flex justify-between py-5 px-5 mb-8 flex-items center pb-0 pl-0 ">
                 <div>
@@ -14,6 +16,11 @@ const CollectionCard = ({ imagepath, text, paragraph }) => {
         
                     <button  className=" text-red-300  border-red-300 border-2 py-5 px-5 float-right mr-16 p-4 mt-4 w-1/3 hover:bg-red-300 text-2xl hover:text-black ">Shop Now</button>
                 </div>
+            </div>
+        </div>
+            <div>
+            <MailContent />
+
             </div>
         </div>
     )
